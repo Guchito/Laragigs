636ee2f06b9c46ac20987363aa5ce07352a621a6
guchito <a_gaggero@hotmail.com>
2025-05-19 18:49:38 +0200

    added controllers

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
-{
-    //
+{ 
+    public function index(){
+        return view('listings.index',[
+        'listings' => Listing::all()
+    ]);
+    }
+    public function show(Listing $listing){
+        return view('listings.show', [
+        'listing' => $listing
+    ]);
+    }
 }
