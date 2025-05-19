6dffc413c03604172541e04d24e3f3ee3f55bb72
guchito <a_gaggero@hotmail.com>
2025-05-19 19:12:28 +0200

    added filter and search

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Listing extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'tags', 'company', 'email', 'website', 'location', 'description'];
+    
+    public function scopeFilter($query, array $filters){
+        if($filters['tag'] ?? false){
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+        if($filters['search'] ?? false){
+            $query->where('title', 'like', '%' . request('search') . '%')
+                  ->orWhere('description', 'like', '%' . request('search') . '%')
+                  ->orWhere('tags', 'like', '%' . request('search') . '%')
+                  ->orWhere('company', 'like', '%' . request('search') . '%')
+                  ->orWhere('location', 'like', '%' . request('search') . '%');
+        }
+    }
 }
